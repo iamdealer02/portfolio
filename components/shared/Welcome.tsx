99810bd3cce7dd70df1d5ee47acc2187ad7d696d
iamdealer02 <sharmaupasana823@gmail.com>
2024-04-04 18:22:33 +0200

    Background Error changes

--- components/shared/Welcome.tsx
+++ components/shared/Welcome.tsx
@@ -3,28 +3,14 @@ import animationData from '@/public/animation.json';
 import Lottie from "lottie-react";
 import NavBar from './NavBar';
 import Links from './Links';
+import TypewriterComponent from 'typewriter-effect';
+
 
 const Welcome = () => {
 
-  const [text, setText] = useState('');
-  const [index, setIndex] = useState(0);
-
-  useEffect(() => {
-    const fullText = "< UPASANA'S PORTFOLIO />";
-    const timer = setInterval(() => {
-      if (index < fullText.length) {
-        setText(prev => prev + fullText[index]); // Append the next character to the text
-        setIndex(prev => prev + 1); // Increment the index
-      } else {
-        clearInterval(timer); // Clear the interval when all characters are displayed
-      }
-    }, 100); // Adjust the delay between characters as needed
-
-    return () => clearInterval(timer); // Cleanup function to clear the interval on unmount
-  }, [index]); 
   
   return (
-    <div className='h-screen '> {/* Added h-screen class to make the div match the screen height */}
+    <div className='h-screen '> 
   <svg className='hidden md:block  absolute top-0 right-0' width="314" height="850" viewBox="0 0 314 850" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M194.883 180.59L228.182 37.1305V1H313V37.1305V815H2L228.182 664.102L189.228 530.206L243.261 347.428L194.883 180.59Z" fill="#2F2F2F" stroke="#505050"/>
   </svg>
@@ -44,7 +30,19 @@ const Welcome = () => {
         </svg>
 
         <div className='font-lexend-giga md:text-4xl text-lg text-center md:mt-4 mt-0'>
-        {text}
+<TypewriterComponent
+    options={{
+        delay: 100,
+        }}
+        onInit={(typewriter) => {
+            const text = "<UPASANA'S PORTFOLIO/>";
+            for (let i = 0; i < text.length; i++) {
+                typewriter.typeString(text[i]);
+            }
+            typewriter.start();
+        }}
+/>
+
         </div>
     </div>
     <div className=' md:grid md:grid-flow-col md:grid-col-3 flex flex-col w-full'>
